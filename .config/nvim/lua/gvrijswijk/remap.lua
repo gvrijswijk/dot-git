aa030831fdaa6a4d4181bfe713433a14a7d1f714
Gerben van Rijswijk <1447552+gvrijswijk@users.noreply.github.com>
2025-10-23 21:01:57 +0200

    A few more basics

--- .config/nvim/lua/gvrijswijk/remap.lua
+++ .config/nvim/lua/gvrijswijk/remap.lua
@@ -3,3 +3,15 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlights text when yanking",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
